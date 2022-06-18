ed1e00d81366e64f31fd3f2cb95c978695d8d00e
Ekaterina <mishelledale@gmail.com>
2022-06-18 12:26:59 +0300

    Task1: Find if the number is palindrome is done

---
+++ Task1/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Введите пятизначное число");
+string number = Console.ReadLine();
+
+int number1 = Convert.ToInt32(number);
+
+void polintop (int number1)
+{
+    if (number1 >= 0)
+    {
+        if (number[0]==number[4] && number[1]==number[3]) Console.WriteLine(number + " является политропом");
+        else
+    {
+        Console.WriteLine(number + " не является политропом");
+    }
+    }
+    if (number1 <0)
+    {
+        if (number[1]==number[5] && number[2]==number[4]) Console.WriteLine(number + " является политропом");
+        else
+    {
+        Console.WriteLine(number + " не является политропом");
+    }
+    }
+}
+
+polintop (number1);
