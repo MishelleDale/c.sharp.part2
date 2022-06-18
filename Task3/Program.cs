da532b580caf0339639506efecc6d01961c7b1de
Ekaterina <mishelledale@gmail.com>
2022-06-18 13:08:55 +0300

    Task3: Find all cubes from 1 to N is done

---
+++ Task3/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Найдем все кубы от 1 до N");
+Console.WriteLine("Введите число N");
+int N = Convert.ToInt32(Console.ReadLine());
+
+void cubes (int N)
+{
+    for (int i = 1; i <= N; i++)
+    {
+        int cube = i*i*i;
+        Console.Write(cube);
+        if (i < N) Console.Write(", ");
+    }
+}
+
+cubes(N);
